a64279c29adb44e71ed1af3067fe55bf44c5f3d8
Mota <131875760+JPedro-Mota@users.noreply.github.com>
2023-11-18 22:05:07 -0300

    Delete Amazon2.0/Amazon2.0/Amazon2.0-1 directory

--- Amazon2.0/Amazon2.0/Amazon2.0-1/js/sugestoes.js
+++
@@ -1,9 +0,0 @@
-<ul id="navbar" class="navbar">
-        <li class="list">celulares</li>
-        <li class="list">fones</li>
-        <li class="list">carregadores</li>
-        <li class="list">notebooks</li>
-        <li class="list">monitores</li>
-        <li class="list">teclados</li>
-        <li class="list">mouses</li>
-      </ul>
